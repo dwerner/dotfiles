e250ee63a69d4b8462c05e08923514e71f2ced57
dwerner <dan.werner@hubblehome.com>
2015-10-13 11:58:45 -0700

    Adding webupd8 jva

--- linux_setup.sh
+++ linux_setup.sh
@@ -8,3 +8,7 @@ sudo mv PowerlineSymbols.otf /usr/share/fonts/
 sudo fc-cache -vf
 sudo mv 10-powerline-symbols.conf /etc/fonts/conf.d/
 
+sudo add-apt-repository ppa:webupd8team/java
+sudo apt-get update
+sudo apt-get -y install oracle-java8-installer
+
