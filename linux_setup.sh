43fe928e26790148a5852a3096319d30e32a5cc4
dwerner <dan.werner@hubblehome.com>
2015-10-20 09:48:50 -0700

    Adding fonts and gradle installation on linux

--- linux_setup.sh
+++ linux_setup.sh
@@ -8,7 +8,17 @@ sudo mv PowerlineSymbols.otf /usr/share/fonts/
 sudo fc-cache -vf
 sudo mv 10-powerline-symbols.conf /etc/fonts/conf.d/
 
+# install oracle's java from ppa
 sudo add-apt-repository ppa:webupd8team/java
 sudo apt-get update
 sudo apt-get -y install oracle-java8-installer
 
+# install gradle from ppa
+sudo add-apt-repository ppa:cwchien/gradle
+sudo apt-get update
+sudo apt-get -y install gradle
+
+# install lxc-docker
+sudo sh -c "echo deb https://get.docker.io/ubuntu docker main > /etc/apt/sources.list.d/docker.list"
+sudo apt-get update
+sudo apt-get -y install lxc-docker
