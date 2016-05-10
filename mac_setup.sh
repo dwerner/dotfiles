04aae9f966f382edb554170c84255ad47045c9dc
Dan Werner <danwerner@Traviss-MacBook-Pro.local>
2016-05-10 09:44:00 -0700

    updated homebrew for mac install

--- mac_setup.sh
+++ mac_setup.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 
 set -e
-ruby -e "$(curl -fsSL https://raw.github.com/Homebrew/homebrew/go/install)"
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
 
 brew install python
 
